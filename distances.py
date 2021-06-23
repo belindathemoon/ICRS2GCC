1e1a126e7730489f894669515b76d256592842b5
Antonia <anto.f.figueroa@gmail.com>
2021-06-23 17:30:48 -0400

    Defined calc_distance function

--- distances.py
+++ distances.py
@@ -3,11 +3,32 @@ import numpy as np
 # temp params used to test the code (copied them from a specific star from the
 # paper)
 
-pm_ra = -2.676
-sigma_pm_ra = 0.043
-pm_dec = -4.991
-sigma_pm_dec = 0.034
-parallax = 0.454
-sigma_parallax = 0.029
+pm_ra_star = -2.676
+sigma_pm_ra_star = 0.043
+pm_dec_star = -4.991
+sigma_pm_dec_star = 0.034
+parallax_star = 0.454
+sigma_parallax_star = 0.029
 
-cov_matrix = np.diag([sigma_pm_ra**2, sigma_pm_dec**2, sigma_parallax**2])
+cov_matrix_star = np.diag([sigma_pm_ra_star**2, sigma_pm_dec_star**2,
+                           sigma_parallax_star**2])
+
+params_star = [pm_ra_star, pm_dec_star, parallax_star]
+
+def calc_distance(params, cov_matrix):
+    parallax = params[2]
+    sigma_parallax = np.sqrt(cov_matrix[2, 2])
+    f = sigma_parallax/sigma_parallax
+    if f > 0.1 or f < 0:
+        distance = method_mcmc(params, cov_matrix)
+    else:
+        distance = method_simple(params, cov_matrix)
+    return distance
+
+def method_simple(params, cov_matrix):
+    print('Using simple method')
+    return 0
+
+def method_mcmc(params, cov_matrix):
+    print('Usign complicated method')
+    return 0
